7ec7f2e5314e5239f2a4ef3ddb0f599a2a65366d
Mike Newton <mike.newton@ed.ac.uk>
2024-11-27 14:55:08 +0000

    Updated main function filename and colour sorting

--- MJN_Colours.m
+++ MJN_Colours.m
@@ -1,7 +1,7 @@
 %---------------------------------------------------------------------------------------------%
 % FUNCTION NAME AND SPECIFICATION
 %
-%   [colVec] = MJN_Plot_Colours
+%   [colVec] = MJN_Colours
 %---------------------------------------------------------------------------------------------%
 % PURPOSE OF THIS FUNCTION:
 %           (1) Calling the function returns a structure containing various lists of nice 
@@ -42,16 +42,18 @@
 % Location (local): [Matlab_root]/LIBRARY/MJN_Code_Library/MJN_Plotting/
 % GitHub location:  https://github.com/self-noise/Matlab_plot_colours
 %---------------------------------------------------------------------------------------------%
-function [colVec] = MJN_Plot_Colours
+function [colVec] = MJN_Colours
 
 % List of 20 agreeable colours (with slight reordering from default)
 % Reference: https://sashamaps.net/docs/resources/20-colors/
-colVec.col = {'#4363d8', '#3cb44b', '#ffe119', '#e6194b', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080', '#ffffff', '#000000'};
+%colVec.col = {'#4363d8', '#3cb44b', '#ffe119', '#e6194b', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080', '#ffffff', '#000000'};
+colVec.col = {'#4363d8', '#3cb44b', '#f58231', '#f032e6', '#46f0f0', '#e6194b', '#911eb4', '#ffe119', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080', '#ffffff', '#000000'};
 
 % The 8 core highlight colours from 'solarized' (reordered to my taste)
 % Reference: https://ethanschoonover.com/solarized/
-colVec.sol8 = {'#268bd2','#cb4b16','#859900','#b58900','#dc322f','#6c71c4','#2aa198','#d33682'};
+%colVec.sol8 = {'#268bd2','#cb4b16','#859900','#b58900','#dc322f','#6c71c4','#2aa198','#d33682'};
+colVec.sol8 = {'#268bd2','#cb4b16','#859900','#b58900','#d33682','#6c71c4','#2aa198','#dc322f'};
 
 % The 8 core monochrome colours from 'solarized' (default ordering)
 % Reference: https://ethanschoonover.com/solarized/
-colVec.solMono = {'#002b36','#073642','#586e75','#657b83','#839496','#93a1a1','#eee8d5','#fdf6e3'};
+colVec.mono = {'#002b36','#073642','#586e75','#657b83','#839496','#93a1a1','#eee8d5','#fdf6e3'};
